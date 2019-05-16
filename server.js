0f54e17b1c8f0566914c290c6371f40f6afa31ae
~A <anilrm07@gmail.com>
2019-05-16 10:03:01 -0700

    disable cors

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ mongoose.connect(process.env.MONGO_URL, function(err) {
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
-app.use(cors())
+// app.use(cors())
 app.use(express.static(path.join(__dirname,'/index.html')))
 app.use('/swal', express.static(__dirname + '/node_modules/sweetalert/dist/'));
 app.use(webpackMiddleware(compiler));
